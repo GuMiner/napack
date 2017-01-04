f8deb0efc5df45ea6c00714770e471bfaf30bee6
Gustave Granroth <gus.gran@gmail.com>
2017-01-04 16:52:32 -0600

    Adding Geolocation DB and starting work on third-party / otherwise attributions.

---
+++ server/Content/docs/ThirdParty.md
@@ -0,0 +1,15 @@
+Third Party Dependencies
+------------------------
+
+The *Napack Framework* depends upon a number of third-party packages.
+
+TODO expand upon this list here.
+
+Copy Left used without modifications, transformations, remixing, or additions
+-----------------------------------------------------------------------------
+* GeoLite2 IP Geolocation database from [MaxMind](http://www.maxmind.com) (CC BY-SA)
+
+
+Atttribution-only or otherwise Free-use Packages
+---------------------------------
+* [Nancy](https://github.com/NancyFx/Nancy) (MIT)
---
+++ server/Definitions/SystemStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Napack.Server
+{
+    /// <summary>
+    /// Holds statistics for the Napack Framework Server system as a whole.
+    /// </summary>
+    public class SystemStats
+    {
+        public SystemStats()
+        {
+            this.RequestStats = new RequestStats();
+        }
+
+        public RequestStats RequestStats { get; set; }
+
+        public Dictionary<string, string> IpToCountry { get; set; }
+
+        public string LookupCountry(string ip)
+        {
+            // TODO the DB is in terms of IP blocks, which will need a lookup algorithm.
+            return string.Empty;
+        }
+    }
+}
